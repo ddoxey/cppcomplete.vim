d6047c0b32851c353812131705609e0150145f15
Dylan Doxey <dylan.doxey@gmail.com>
2021-11-15 21:51:37 -0800

    adding unittests

--- bin/cppcomplete.py
+++ bin/cppcomplete.py
@@ -4,93 +4,249 @@ import os
 import sys
 import string
 import subprocess
+import tracemalloc
+
+tracemalloc.start()
 
 MAN = '/usr/bin/man'
 
-def get_man(thing):
+class CPPComplete:
 
-    p = subprocess.Popen(f'{MAN} -P cat {thing}',
-                         shell=True,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
+    PACKAGE_RE     = '\w+(?: ::\w+ )*'
+    METHOD_CALL_RE = '\w+[.]\w+[(][^)]*[)]'
 
-    for line in p.stdout.readlines():
-        line = re.sub(r'\x08.', '', line.decode(sys.stdout.encoding).rstrip())
-        if len(line) == 0:
-            continue
-        yield line
+    @classmethod
+    def get_man(cls, thing):
+
+        p = subprocess.Popen([MAN, '-P', 'cat', thing],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+        output = []
+        for line in p.stdout.readlines():
+            line = re.sub(r'\x08.', '', line.decode(sys.stdout.encoding).rstrip())
+            if len(line) == 0:
+                continue
+            if line.startswith('No manual entry for'):
+                break
+            output.append(line)
+        p.wait()    
+        p.stdout.close()    
+        return output
 
 
-def get_object_classname(filename, thing):
+    @classmethod
+    def search_file(cls, filename, line_number, regexes):
 
-    declare_regex = re.compile(f'(\S+)[ ]{thing};')
+        with open(filename, 'r') as fh:
+            line_n = 0
+            for line in fh:
+                line_n += 1
+                if line_n == line_number:
+                    break
+                for regex in regexes:
+                    m = regex.search(line)
+                    if m is not None:
+                        yield m.groups()
 
-    fh = open(filename, 'r')
-    lines = reversed(list(fh))
 
-    for line in lines:
-        m = declare_regex.search(line)
-        if m is not None:
-            return m.group(1)
+    @classmethod
+    def find_first_in_file(cls, filename, line_number, regexes):
 
-    return None
+        lines = []
 
-def get_class_members(classname):
+        with open(filename, 'r') as fh:
+            for line in fh:
+                lines.append(line.rstrip())
+                if line_number > 0 and len(lines) == line_number:
+                    break
 
-    disregard = [
-        "",
-        'constructor',
-        'destructor',
-    ]
+        for line_i, line in enumerate(lines):
+            line_n = line_i + 1
+            for regex in regexes:
+                m = regex.search(line)
+                if m is not None:
+                    return line_n, m.group(1)
 
-    ignoring = True
-    member_regex = re.compile(r'^          ([a-z][a-z_]*[+=<>()]*)[ ]')
+        return None, None
 
-    for line in get_man(classname):
 
-        if line.lower().startswith('non-member '):
-            break
+    @classmethod
+    def find_include(cls, dirname, basename):
+        candidates = [
+            os.path.join(dirname, basename),
+            os.path.join('/var/include', basename),
+        ]
+        for candidate in candidates:
+            if os.path.exists(candidate):
+                return candidate
+        return None
 
-        if ignoring == True and line.lower().startswith('member functions'):
-            ignoring = False
 
-        if ignoring == True:
-            continue
+    @classmethod
+    def find_usings(cls, filename, line_number):
 
-        m = member_regex.search(line)
+        usings_regexes = [
+            re.compile(r'using \s+ ( \w+ ) \s* ;', re.X|re.M|re.S),
+            re.compile(r'using \s+ ( \w+ ) \s* = \s* ({PACKAGE_RE}) \s* ;', re.X|re.M|re.S),
+        ]
+        usings = cls.search_file(filename, line_number, usings_regexes)
 
-        if m is not None:
+        for using in [u for u in usings]:
 
-            member = m.group(1)
+            if len(using) == 1:
+                yield using[0], using[0]
+            else:
+                yield using
 
-            if member.startswith('operator'):
+
+    @classmethod
+    def normalize_with_usings(cls, classname, filename, line_number):
+
+        usings = cls.find_usings(filename, line_number)
+
+        if usings is not None:
+
+            for using in usings:
+
+                if using[0] == classname:
+                    return using[1]
+                elif cls.symbol_is_in(classname, using[0]):
+                    return f'{using[0]}::{classname}'
+
+        return classname
+
+
+    @classmethod
+    def symbol_is_in(cls, symbol, namespace):
+        lines = cls.get_man(f'{namespace}::{symbol}')
+        return len(lines) > 0
+
+
+    @classmethod
+    def get_expression_classname(cls, filename, line_number, expression):
+
+        dirname = os.path.dirname(filename)
+
+        include_regex = re.compile(r'[#]include \s+ ["]( [^"]+ )["]', re.X|re.M|re.S)
+
+        includes = cls.search_file(filename, line_number, [include_regex])
+
+        for include in [i[0] for i in includes]:
+            include_path = cls.find_include(dirname, include)
+            if include_path is not None:
+                definitions_path = os.path.splitext(include_path)[0] + '.cpp'
+                if os.path.exists(definitions_path):
+                    classname = cls.get_object_classname(definitions_path, 0, expression)
+                else:
+                    classname = cls.get_object_classname(include_path, 0, expression)
+
+                if classname is not None:
+                    return cls.normalize_with_usings(classname, filename, line_number);
+
+        return None
+
+
+    @classmethod
+    def get_object_classname(cls, filename, line_number, thing):
+
+        thing_re = re.sub(r'(\W)', r'[\1]', thing)
+
+        regexes = [
+            # thing = some::thing(
+            re.compile(f'\W {thing} \s* = \s* ({cls.PACKAGE_RE})[(].+[)];', re.X|re.M|re.S),
+            # some::thing thing;
+            re.compile(f'({cls.PACKAGE_RE}) \s+ {thing_re} \s* [;{{]', re.X|re.M|re.S),
+        ]
+
+        _, classname = cls.find_first_in_file(filename, line_number, regexes)
+
+        if classname is not None:
+            return cls.normalize_with_usings(classname, filename, line_number);
+
+        regexes = [
+            # auto thing = something;
+            re.compile(f'auto \s+ {thing} \s* = \s* (\w+);', re.X|re.M|re.S),
+            # auto thing = something();
+            re.compile(f'auto \s+ {thing} \s* = \s* (\w+[(].*[)]);', re.X|re.M|re.S),
+            # auto thing = something.get_thing();
+            re.compile(f'auto \s+ {thing} \s* = \s* ({cls.METHOD_CALL_RE})', re.X|re.M|re.S),
+        ]
+
+        find_line_n, origin = cls.find_first_in_file(filename, line_number, regexes)
+
+        if origin is None:
+            return None
+
+        if origin is not None:
+            if '.' not in origin and '->' not in origin:
+                classname = cls.get_object_classname(filename, find_line_n, origin)
+            else:
+                classname = cls.get_object_classname(filename, find_line_n, origin)
+
+            if classname is not None:
+                return cls.normalize_with_usings(classname, filename, line_number);
+
+        return cls.get_expression_classname(filename, find_line_n, origin)
+
+
+    @classmethod
+    def get_class_members(cls, classname):
+
+        disregard = [
+            "",
+            'constructor',
+            'destructor',
+        ]
+
+        ignoring = True
+        member_regex = re.compile(r'^          ([a-z][a-z_]*[+=<>()]*)[ ]')
+
+        for line in cls.get_man(classname):
+
+            if line.lower().startswith('non-member '):
+                break
+
+            if ignoring == True and line.lower().startswith('member functions'):
+                ignoring = False
+
+            if ignoring == True:
                 continue
 
-            if member in disregard:
-                continue
+            m = member_regex.search(line)
 
-            yield member
+            if m is not None:
+
+                member = m.group(1)
+
+                if member.startswith('operator'):
+                    continue
+
+                if member in disregard:
+                    continue
+
+                yield member
 
 
-def run(filename, thing):
+    @classmethod
+    def search(cls, filename, line_number, thing):
 
-    classname = get_object_classname(filename, thing.strip())
+        classname = cls.get_object_classname(filename, line_number, thing.strip())
 
-    if classname is None:
-        return
+        if classname is None:
+            return
 
-    members = get_class_members(classname)
+        members = cls.get_class_members(classname)
 
-    if members is None:
-        return
-
-    for member in members:
-        print(f'{thing}.{member}')
+        if members is not None:
+            for member in members:
+                print(f'{thing}.{member}')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         if not os.path.exists(filename):
             raise Exception(f'No such file: {filename}')
-        run(filename, sys.argv[2])
+        if not sys.argv[2].isdigits():
+            raise Exception(f'Not a line number: {sys.argv[2]}')
+        CPPComplete.search(filename, sys.argv[2], sys.argv[3])
---
+++ test/cppcomplete_test.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+from cppcomplete import CPPComplete
+import os
+import unittest
+from tempfile import NamedTemporaryFile
+
+
+class CPPArtifact:
+
+    others = []
+
+    def __init__(self, suffix, text):
+        self.file = NamedTemporaryFile(delete=False, suffix=f'.{suffix}')
+        self.file.write(text.strip().encode('UTF-8'))
+        self.file.close()
+
+    def __del__(self):
+        os.remove(self.file.name)
+        for other in self.others:
+            if os.path.exists(other):
+                os.remove(other)
+
+    def touch(self, ext):
+        other = os.path.splitext(self.file.name)[0] + f'.{ext}'
+        open(other, 'a').close()
+        self.others.append(other)
+        return os.path.basename(other)
+
+    @property
+    def filename(self):
+        return self.file.name
+
+    @property
+    def basename(self):
+        return os.path.basename(self.file.name)
+
+
+class TestCPPCompleteGetObjectClassname(unittest.TestCase):
+
+    def test_basic_declaration_a(self):
+
+        sample_cpp = CPPArtifact('cpp', "std::string foobar;")
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 1, "foobar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_basic_declaration_b(self):
+
+        sample_cpp = CPPArtifact('cpp', "auto foobar = std::string(\"\");")
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 1, "foobar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_copy(self):
+
+        sample_cpp = CPPArtifact('cpp',
+        """
+            std::string foo;
+            auto bar = foo;
+        """)
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 2, "bar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_local_function_return(self):
+
+        sample_cpp = CPPArtifact('cpp',
+        """
+            std::string foo();
+            auto bar = foo();
+        """)
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 2, "bar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_include_function_return_a(self):
+
+        sample_hpp = CPPArtifact('hpp',
+        """
+            std::string foo();
+        """)
+
+        sample_cpp = CPPArtifact('cpp',
+        """
+            #include "{}"
+            auto bar = foo();
+        """.format(sample_hpp.basename))
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 2, "bar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_include_function_return_b(self):
+
+        definition_cpp = CPPArtifact('cpp',
+        """
+            std::string foo() { return "bar" };
+        """)
+
+        hpp_basename = definition_cpp.touch('hpp')
+
+        sample_cpp = CPPArtifact('cpp',
+        """
+            #include "{}"
+            auto bar = foo();
+        """.format(hpp_basename))
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 2, "bar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+    def test_using_namespace_a(self):
+
+        sample_cpp = CPPArtifact('cpp',
+        """
+            using std;
+
+            string foobar;
+        """)
+
+        classname = CPPComplete.get_object_classname(sample_cpp.filename, 3, "foobar")
+
+        self.assertEqual(classname, 'std::string')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
